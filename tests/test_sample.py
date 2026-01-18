1cb2766a76202a6c294e2ab725117014d210891f
Karthik KY <kingkarthik1995@gmail.com>
2026-01-18 14:42:58 +0530

    adding env variables

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,11 @@
+import os
+
 def test_addition():
     assert 1 + 1 == 2
 
 def test_addition_fail():
     assert 1 + 1 == 3
+
+def test_base_url():
+    base_url = os.getenv("BASE_URL")
+    assert base_url is not None
